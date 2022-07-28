0d2bf3ed521f1979eb77d84eae9a7f7454dcce56
OraCl13 <ostap.pelo@gotoinc.co>
2022-07-28 10:46:40 +0300

    lesson 9 start

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,11 +1,21 @@
 class AnswersController < ApplicationController
-  before_action :authenticate_user!, only: [ :create, :destroy ]
+  before_action :authenticate_user!, only: [:create, :destroy]
   # before_action :load_question, only: [ :new, :create ]
   # before_action :load_answer, only: [ :edit, :update, :destroy]
 
   def create
     @question = Question.find(params[:question_id])
-    @answer = @question.answers.create(answer_params.merge(user_id: current_user.id))
+    @answer = @question.answers.build(answer_params.merge(user_id: current_user.id))
+
+    respond_to do |format|
+      if @answer.save
+        format.html { render partial: 'questions/answers', layout: false }
+        format.json { render json: @answer }
+      else
+        format.html { render text: @answer.errors.full_messages.join("\n"), status: :unprocessable_entity }
+        format.json { render text: @answer.errors.full_messages, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
