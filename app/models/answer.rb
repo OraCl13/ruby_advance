b6ac3203c43032e47f6556b62d7dc05cb0373b6a
OraCl13 <ostap.pelo@gotoinc.co>
2022-08-01 14:34:57 +0300

    update rails version to 5.0.0.1

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,4 @@
-class Answer < ActiveRecord::Base
+class Answer < ApplicationRecord
   validates :body, :reply_to_id, presence: true
 
   belongs_to :reply_to, class_name: 'Question', foreign_key: :reply_to_id
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,4 @@
-class Question < ActiveRecord::Base
+class Question < ApplicationRecord
   validates :body, :title, presence: true
 
   has_many :answers, foreign_key: :reply_to_id, dependent: :destroy
