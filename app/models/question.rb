b9fc1d57e115c88ec66100d043bd1a1e21572bb8
OraCl13 <ostap.pelo@gotoinc.co>
2022-08-15 16:24:30 +0300

    test

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,13 @@ class Question < ApplicationRecord
   has_many :comments, dependent: :destroy, as: :article
 
   accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
+
+  after_create :calculate_reputation
+
+  private
+
+  def calculate_reputation
+    reputation = Reputation.calculate(self)
+    User.find(self.user_id).update(reputation: reputation)
+  end
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -4,17 +4,8 @@ require 'rails_helper'
 
 describe 'Questions API' do
   describe 'GET /index' do
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get '/api/v1/questions', format: :json
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/questions', params: { format: :json, access_token: '1234' }
-        expect(response.status).to eq 401
-      end
-    end
+    # let(:api_path) { get '/api/v1/questions', access_token: '1234', format: :json }
+    it_behaves_like 'API Authenticable'
 
     context 'authorized' do
       let(:access_token) { create(:access_token) }
@@ -55,4 +46,7 @@ describe 'Questions API' do
       end
     end
   end
-end
+  def do_request(options = {})
+    get '/api/v1/questions', params: { format: :json }.merge(options)
+  end
+end 
