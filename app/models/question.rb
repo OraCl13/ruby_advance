cfe41d33e46a753b4f7f6b8b5ea7adea2b8be899
OraCl13 <ostap.pelo@gotoinc.co>
2022-08-16 10:33:02 +0300

    err

--- app/models/question.rb
+++
@@ -1,9 +0,0 @@
-class Question < ApplicationRecord
-  validates :body, :title, presence: true
-
-  has_many :answers, foreign_key: :reply_to_id, dependent: :destroy
-  has_many :attachments, dependent: :destroy, as: :attachmentable
-  has_many :comments, dependent: :destroy, as: :article
-
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
-end
