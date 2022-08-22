cadaee021cf9f8522c47e0b462b0a7ef717e4061
OraCl13 <ostap.pelo@gotoinc.co>
2022-08-22 10:18:33 +0300

    refactor

--- app/jobs/message_for_answer_job.rb
+++ app/jobs/message_for_answer_job.rb
@@ -6,9 +6,9 @@ class MessageForAnswerJob < ApplicationJob
                when 'com_Question'
                  Comment.find(args[0]).article_id
                when 'com_Answer'
-                 Answer.find(Comment.find(args[0]).article_id).reply_to
+                 Answer.find(Comment.find(args[0]).article_id).reply_to.id
                else
-                 Answer.find(args[0]).reply_to
+                 Answer.find(args[0]).reply_to.id
                end
     Question.find(question).subscribers.each do |subscriber_id|
       NewMessageMailer.message(User.find(subscriber_id).email).deliver_later
