26142376ad4f286544e0a43531461a9620d9c232
OraCl13 <ostap.pelo@gotoinc.co>
2022-08-23 15:06:57 +0300

    done search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,23 @@
 class SearchesController < ApplicationController
   def result
-    @general = ThinkingSphinx.search search_params[:request]
+    @general = []
+    return @general = ThinkingSphinx.search(search_params) unless additional_params.values.include? '1'
+
+    additional_params.each do |key, value|
+      obj = Object.const_get(key)
+      obj.connection
+      @general += obj.search(search_params).to_a if value == '1'
+    end
+    @general
   end
 
   private
 
   def search_params
-    params.require(:user).permit(:request)
+    params[:user][:request]
+  end
+
+  def additional_params
+    params[:user].except :request
   end
 end
