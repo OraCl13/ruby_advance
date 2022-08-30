d9bf00d92268e661bf01a6f32b3efc2b345a639c
OraCl13 <ostap.pelo@gotoinc.co>
2022-08-30 12:16:55 +0300

    1

--- config/initializers/redis.rb
+++
@@ -1,9 +0,0 @@
-require 'redis'
-
-REDIS_CONFIG = YAML.load(File.open(Rails.root.join("config/redis.yml"))).symbolize_keys
-dflt = REDIS_CONFIG[:default].symbolize_keys
-cnfg = dflt.merge(REDIS_CONFIG[Rails.env.to_sym].symbolize_keys) if REDIS_CONFIG[Rails.env.to_sym]
-
-$redis = Redis.new(cnfg)
-Redis::Objects.redis = $redis
-$redis.flushdb if (Rails.env = "test")
