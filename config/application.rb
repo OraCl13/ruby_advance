c7e26b0c325badbd3af1dee9063105c43547cfae
OraCl13 <ostap.pelo@gotoinc.co>
2022-08-30 15:31:03 +0300

    Sendgrid smtp connected

--- config/application.rb
+++ config/application.rb
@@ -2,18 +2,14 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Qna2
   class Application < Rails::Application
-    # Use the responders controller from the responders gem
     config.app_generators.scaffold_controller :responders_controller
 
     config.active_job.queue_adapter = :sidekiq
 
-    # config.active_record.include_root_in_json = true
     config.generators do |g|
       g.test_framework :rspec,
                        fixtures: true,
