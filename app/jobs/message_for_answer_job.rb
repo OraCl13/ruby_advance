d13ee5d1f16b8db47732077a710a02791cc0a248
OraCl13 <ostap.pelo@gotoinc.co>
2022-09-01 15:18:07 +0300

    debug

--- app/jobs/message_for_answer_job.rb
+++ app/jobs/message_for_answer_job.rb
@@ -2,6 +2,8 @@ class MessageForAnswerJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    puts 'WE ARE IN PERFOrM'
+    puts 'x' * 1000
     question = case args[-1]
                when 'com_Question'
                  Comment.find(args[0]).article_id
@@ -10,7 +12,10 @@ class MessageForAnswerJob < ApplicationJob
                else
                  Answer.find(args[0]).reply_to.id
                end
+    puts question
     Question.find(question).subscribers.each do |subscriber_id|
+      puts User.find(subscriber_id).email
+      puts 'Y' *1000
       NewMessageMailer.message(User.find(subscriber_id).email).deliver_later
     end
   end
