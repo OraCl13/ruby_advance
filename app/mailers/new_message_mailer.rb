d5d719e4ca264023713f64a1ffeb9a388586f84b
OraCl13 <ostap.pelo@gotoinc.co>
2022-09-01 15:41:21 +0300

    sidekiq for mailer production

--- app/mailers/new_message_mailer.rb
+++ app/mailers/new_message_mailer.rb
@@ -1,8 +1,6 @@
 class NewMessageMailer < ApplicationMailer
   def message(email)
     @greeting = 'You have new answer for yours question'
-    puts 'WE ARE IN MAIL'
     mail to: email, subject: 'Welcome to My Awesome Site'
-    puts 'SENDED'
   end
 end
