a9436103bcbdc9f0202a1fdf55bf3d81148023cb
OraCl13 <ostap.pelo@gotoinc.co>
2022-09-01 15:34:58 +0300

    debug

--- app/mailers/new_message_mailer.rb
+++ app/mailers/new_message_mailer.rb
@@ -1,6 +1,8 @@
 class NewMessageMailer < ApplicationMailer
   def message(email)
     @greeting = 'You have new answer for yours question'
+    puts 'WE ARE IN MAIL'
     mail to: email, subject: 'Welcome to My Awesome Site'
+    puts 'SENDED'
   end
 end
